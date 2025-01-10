02e991d94cce513cb413331faaa70ec117eae802
Skyler Robbins <ssssssssssssss>
2025-01-10 09:03:04 -0700

    Completed all parts

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct Emoji: Codable, Identifiable, Equatable {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    var id: String {
+        symbol
+    }
+    
+    var sectionTitle: String {
+        String(name.uppercased().first ?? "?")
+    }
+
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
